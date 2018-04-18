c552e9b6b5276fb8d4a5fc28f9fe086503c1a3c1
Gabriele Girelli <gigi.ga90@gmail.com>
2018-04-18 14:02:44 +0200

    Setting up unit tests.

---
+++ gpseq_ce/test/test_bed.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+'''
+@author: Gabriele Girelli
+@contact: gigi.ga90@gmail.com
+@description: bed-related methods.
+'''
+
+# DEPENDENCIES =================================================================
+
+import numpy as np
+import os
+import pandas as pd
+import pybedtools as pbt
+
+from gpseq_ce import bed
+
+# PARAMS =======================================================================
+
+# Bed file, with names and value, bin size 50, bin step 25, chr-size 100
+bedstr  = "1\t0\t50\tr1\t1\n"
+bedstr += "1\t25\t75\tr2\t2\n"
+bedstr += "1\t50\t100\tr3\t4\n"
+bedstr += "1\t75\t125\tr4\t5\n"
+
+# Expected windows from bedstr, with {1:100} chr-size
+winstr  = "1\t0\t50\n"
+winstr += "1\t25\t75\n"
+winstr += "1\t50\t100\n"
+winstr += "1\t75\t125\n"
+
+# Bins with bin size == bin step = 50, chr-size = 100
+binstr  = "1\t0\t50\n"
+binstr += "1\t50\t100\n"
+
+# Expected binning of bedstr with binstr
+binnedstr  = "1\t0\t50\t1\n"
+binnedstr += "1\t0\t50\t2\n"
+binnedstr += "1\t50\t100\t4\n"
+binnedstr += "1\t50\t100\t5\n"
+
+# FUNCTIONS ====================================================================
+
+def test_mkWindows():
+    b = bed.mk_windows({1:100}, 50, 25)
+    with open(b.fn, "r+") as IH:
+        content = "".join(IH.readlines())
+    assert content == winstr
+
+def test_getChrSize():
+    b = pbt.BedTool(bedstr, from_string = True)
+    assert bed.get_chr_size(b.fn) == {"1" : 125}
+
+def test_toBins():
+    a = pbt.BedTool(binstr, from_string = True)
+    b = pbt.BedTool(bedstr, from_string = True)
+    binned = bed.to_bins(a, b)
+    with open(binned.fn, "r+") as IH:
+        content = "".join(IH.readlines())
+    assert content == binnedstr
+
+def test_toCombinedBins():
+    pass
+
+def test_calcStats():
+    pass
+
+# END ==========================================================================
+
+################################################################################
