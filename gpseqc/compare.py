af8f228254eed5804956ebc4d10d408417b14dac
Gabriele Girelli <gigi.ga90@gmail.com>
2018-04-25 17:22:14 +0200

    Implemented legacy way of calculating KT and KTw.

--- gpseqc/compare.py
+++ gpseqc/compare.py
@@ -8,8 +8,11 @@
 
 # DEPENDENCIES =================================================================
 
+import numpy as np
 import os
 import pandas as pd
+
+from memory_profiler import profile
 
 from gpseqc.centrality import CMETRICS
 
@@ -45,15 +48,11 @@ class RankTable(object):
         elif type(None) != type(df):
             self._df = df
         else:
-            assert_msg = "ipath or df are required to construct a RankTable"
-            raise AssertError(assert_msg)
+            self._df = pd.DataFrame(columns = ["chrom", "start", "end"])
 
         req_cols = ["chrom", "start", "end"]
         for c in req_cols:
             assert c in self._df.columns, "missing column: '%s'" % c
-
-        assert self._df.shape[1] > 3, "no metrics found"
-        assert self._df.shape[0] > 0, "empty rank table"
 
         assert_msg = "found duplicate regions"
         assert len(set(self._all_regions())) == self._df.shape[0], assert_msg
@@ -106,15 +105,21 @@ class RankTable(object):
         i.e. all region intervals that are in both RankTables with the rankings
         from the first (self)."""
 
+        assert_msg = "RankTable expected, got '%s'" % type(b)
+        assert type(RankTable()) == type(b), assert_msg
+
         # Identify intersection
         iset = set(self.iter_regions()).intersection(set(b.iter_regions()))
         if 0 == len(iset): return(None)
 
         # Copy current dataframe and subset it
         df = self._df.copy()
+        regs = self._all_regions()
         for i in range(self._df.shape[0]):
-            if tuple(self._df.iloc[i, :].tolist()[:3]) not in iset:
-                df.drop(i)
+            if regs[i] not in iset:
+                df = df.drop(i)
+        df.index = range(df.shape[0])
+        del regs
 
         return(RankTable(df = df))
 
@@ -123,7 +128,6 @@ class RankTable(object):
         i.e. all region intervals that are in both RankTables with the rankings
         from the first (self). Same as self & b.'''
         return(self & b)
-
 
 class MetricTable():
     '''Instance of a metric table, with 4 columns: chr, start, end, metric.
@@ -136,21 +140,24 @@ class MetricTable():
     _df = None
     _metric = None
 
-    def __init__(self, df):
+    def __init__(self, df = None):
         '''Build metric table instance from pd.DataFrame.
 
         Args:
             df (pd.DataFrame): a metric table extracted from a RankTable.
         '''
         
-        self._df = df
-        self._df.columns.values[:3] = ["chrom", "start", "end"]
-        self._metric = self._df.columns[3]
+        if type(None) == type(df):
+            self._df = pd.DataFrame(columns = [
+                "chrom", "start", "end", list(CMETRICS.keys())[0]])
+        else:
+            self._df = df
 
         assert_msg = "expected 4 columns, got %d" % self._df.shape[1]
         assert self._df.shape[1] == 4, assert_msg
 
-        assert self._df.shape[0] > 0, "empty metric table"
+        self._df.columns.values[:3] = ["chrom", "start", "end"]
+        self._metric = self._df.columns[3]
 
         assert_msg = "unrecognized metric '%s'" % self._df.columns[-1]
         assert_msg += "\nAvailable: %s" % str(list(CMETRICS.keys()))
@@ -160,6 +167,14 @@ class MetricTable():
         assert len(set(zip(self._df.iloc[:, 0], self._df.iloc[:, 1],
             self._df.iloc[:, 2]))) == self._df.shape[0], assert_msg
 
+        # Remove np.nan and np.inf
+        self._df = self._df.drop(np.where(np.isnan(self.mcol.astype('f')))[0])
+        self._df = self._df.drop(np.where(np.isinf(self.mcol.astype('f')))[0])
+
+        # Sort (rank)
+        self._df = self._df.sort_values(self._metric)
+        self._df.index = range(self._df.shape[0])
+        
     def __getitem__(self, i):
         '''Return the i-th region.'''
         if i >= len(self):
@@ -183,6 +198,10 @@ class MetricTable():
     def metric(self):
         return self._metric
 
+    @property
+    def mcol(self):
+        return self._df.iloc[:, 3]
+
     def iter_regions(self):
         '''Yields regions.'''
         for r in self._all_regions():
@@ -201,15 +220,21 @@ class MetricTable():
         i.e. all region intervals that are in both MetricTables with the
         rankings from the first (self)."""
 
+        assert_msg = "MetricTable expected, got '%s'" % type(b)
+        assert type(MetricTable()) == type(b), assert_msg
+
         # Identify intersection
         iset = set(self.iter_regions()).intersection(set(b.iter_regions()))
         if 0 == len(iset): return(None)
 
         # Copy current dataframe and subset it
         df = self._df.copy()
+        regs = self._all_regions()
         for i in range(self._df.shape[0]):
-            if tuple(self._df.iloc[i, :].tolist()[:3]) not in iset:
-                df.drop(i)
+            if regs[i] not in iset:
+                df = df.drop(i)
+        df.index = range(df.shape[0])
+        del regs
 
         return(MetricTable(df = df))
 
@@ -219,7 +244,88 @@ class MetricTable():
         rankings from the first (self). Same as self & b.'''
         return(self & b)
 
+    @profile
+    def KendallTau(self, b, skipSubset = False):
+        '''Calculate Kendall tau distance between two MetricTables.
+        The distance is calculated only on the intersection between the tables.
 
+        Args:
+            b (MetricTable): .
+            skipSubset (bool): .
+        '''
+
+        # Apply subsetting if needed
+        if not skipSubset:
+            a = self.intersection(b)
+            b = b.intersection(a)
+
+        # Calculate elements indexes
+        idx = np.array([a._df.index.tolist(),
+            np.array([a._all_regions().index(x) 
+                for x in b.iter_regions()])], dtype = "i").transpose()
+        print(2)
+        print(idx.shape)
+
+        # Identify all possible couples
+        exg = np.array([(x, y) for x in idx[:,0] for y in idx[:,0]])
+        print(3)
+
+        # Calculate number of discordant orders
+        n = (np.array(idx[exg[:,0],0]) > np.array(idx[exg[:,1],0])).astype('i')
+        print(4)
+        n += (np.array(idx[exg[:,0],1]) > np.array(idx[exg[:,1],1])).astype('i')
+        print(5)
+        n = sum(n == 1) / 2.
+
+        # Normalize
+        d = n / (len(a) * (len(a) - 1) / 2.)
+
+        # Output
+        return d
+
+    def KendallTau_weighted(self, b, skipSubset = False):
+        '''Calculate Kendall tau distance between two MetricTables.
+        The distance is calculated only on the intersection between the tables.
+
+        Args:
+            b (MetricTable): .
+            skipSubset (bool): .
+        '''
+
+        # Apply subsetting if needed
+        if not skipSubset:
+            a = self.intersection(b)
+            b = b.intersection(self)
+
+        # Calculate elements indexes
+        idx = np.array([self._df.index, [self._all_regions().index(x) 
+            for x in b.iter_regions()]], dtype = "i").transpose()
+
+        # Identify all possible couples
+        exg = np.array([(x, y) for x in idx[:,0] for y in idx[:,0]])
+
+        # Identify discordant orders
+        disc1 = np.array(idx[exg[:,0],0]) > np.array(idx[exg[:,1],0])
+        disc2 = np.array(idx[exg[:,0],1]) > np.array(idx[exg[:,1],1])
+        disc = (disc1.astype('i') + disc2.astype('i')) == 1
+
+        # Calculate weights
+        def calc_weight(r, e):
+            v1 = r['value'].values[e[:,0]].astype('f')
+            v2 = r['value'].values[e[:,1]].astype('f')
+            w = abs(v1 - v2) / sum(abs(v1 - v2))
+            return(w)
+        w1 = calc_weight(r1, exg)
+        w2 = calc_weight(r2, exg)
+
+        # Calculate sum of discordant orders weights
+        n = sum((w1[disc] + w2[disc]) / 2.)
+
+        # Normalize
+        d = n / (sum(w1 + w2) / 2.)
+
+        # Output
+        return d
 
 # END ==========================================================================
 
